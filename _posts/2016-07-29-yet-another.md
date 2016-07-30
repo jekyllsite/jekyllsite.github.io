94522b6ecbada77de336dcfc2c993f6265ceec11
c <c@c-MacBook>
2016-07-30 01:45:41 -0400

    updating

---
+++ _posts/2016-07-29-yet-another.md
@@ -0,0 +1,7 @@
+---
+title: "Otro de los tests"
+layout: post
+date: 2016-07-29
+---
+
+**Y** este post, es otro de los muchos 
---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git add -A #git add all files 
+
+git commit -am'updating' # commit messages and update
+
+git push # git push changes 
